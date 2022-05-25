027b010a0e13ce91ad67c8974a4337270c632765
kuttalam <kuttalambc@gmail.com>
2022-05-25 13:13:44 +0530

    adminepannel commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import DashBoard from './DashBoard';
 import logo from './logo.svg';
-import './App.css';
+import Sidebar from './Sidebar';
+import Topbar from './Topbar';
+import Userlist from './Userlist';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Userform from './Userform'
+import Useredit from './Useredit'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div id="wrapper">
+        <Sidebar />
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Topbar />
+            <div class="container-fluid">
+              
+                <Routes>
+                  
+                  <Route path="/" element={<DashBoard />}></Route>
+                  <Route path="/users" element={<Userlist />}> </Route>
+                  <Route path="/usersform" element={<Userform />}> </Route>
+                  <Route path="/edit/:id" element={<Useredit />}> </Route>
+                 
+                </Routes>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App; 
